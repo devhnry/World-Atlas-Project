cb361cc6aec84b9778a00e03506a6412fdf44bc9
devhnry <devwhenry@gmail.com>
2023-12-27 13:55:32 +0100

    Built Search Input Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import Header from "./components/Header";
+import SearchInput from "./components/SearchInput";
 import "./index.css";
 
 function App() {
-
   return (
     <>
       <Header />
+      <section className="px-4">
+        <SearchInput />
+      </section>
     </>
   );
 }
