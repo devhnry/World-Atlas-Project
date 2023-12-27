cb91be0039b08f1fab2b7b1cbc64988c4c775ab9
devhnry <devwhenry@gmail.com>
2023-12-27 15:16:49 +0100

    Updated styling

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -7,14 +7,14 @@ const SearchInput = () => {
         onSubmit={(e) => {
           e.preventDefault();
         }}
-        className="flex gap-6 items-center bg-white mt-6 xs:mt-8 sm:mt-10 lg:mt-12 py-4 px-8 shadow-base rounded-md max-w-[480px] w-full"
+        className="flex gap-6 items-center bg-white dark:bg-blue-dark-400 mt-6 xs:mt-8 sm:mt-10 lg:mt-12 py-4 px-8 shadow-base rounded-md max-w-[480px] w-full"
       >
-        <FaSearch className="text-gray-350" />
+        <FaSearch className="text-gray-350 dark:text-white" />
         <input
           title="searchInput"
           type="text"
           placeholder="Search for a country..."
-          className="placeholder:text-sm placeholder:text-gray-300 outline-none"
+          className="placeholder:text-sm sm:placeholder:text-base placeholder:text-gray-300 outline-none bg-transparent dark:placeholder:text-white"
         />
       </form>
     </div>
