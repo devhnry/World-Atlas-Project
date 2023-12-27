2b55ebac33b5135b6dc70521bb5b1809184bf967
devhnry <devwhenry@gmail.com>
2023-12-27 10:05:27 +0100

    Setting Up Tailwind Config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      nunito: ["Nunito Sans", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        blue: {
+          "dark-400": "hsl(209, 23%, 22%)",
+          "dark-500": "hsl(207, 26%, 17%)",
+          "dark-900": "hsl(200, 15%, 8%)",
+        },
+        gray: {
+          100: "hsl(0, 0%, 98%)",
+          400: "hsl(0, 0%, 52%)",
+        },
+      },
+    },
   },
   plugins: [],
 };
